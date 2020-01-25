41a79a9e3868aa6e42a365660360ff1c0aa9c976
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-01-25 15:15:33 +0530

    added abeliophyllum.html

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const Response = require('./lib/response');
 const CONTENT_TYPES = require('./lib/mimeTypes');
 const STATIC_FOLDER = `${__dirname}/public`;
 
-const serveStaticFile = (req) => {
-  const path = `${STATIC_FOLDER}${req.url}`;
+const serveStaticFile = (req, optionalUrl) => {
+  const path = `${STATIC_FOLDER}${optionalUrl || req.url}`;
   const stat = fs.existsSync(path) && fs.statSync(path);
   if (!stat || !stat.isFile()) return new Response();
   const [, extension] = path.match(/.*\.(.*)$/) || [];
@@ -18,11 +18,13 @@ const serveStaticFile = (req) => {
   return res;
 }
 
+const serveHomePage = function(req) {
+  return serveStaticFile(req, '/home.html');
+};
+
 const findHandler = (req) => {
-  if(req.method === 'GET') {
-    if(req.url == '/') req.url = '/home.html';
-    return serveStaticFile;
-  };
+  if(req.method === 'GET' && req.url === '/') return serveHomePage;
+  if(req.method === 'GET') return serveStaticFile;
   return () => new Response();
 }
 const processRequest = (req) => {
