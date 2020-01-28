a9154843035621b3471a69d5442e7d03b6670cfd
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-01-28 19:00:31 +0530

    updated server from net to http

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 const fs = require('fs');
-const Response = require('./lib/response');
 const CONTENT_TYPES = require('./lib/mimeTypes');
 const {loadTemplate} = require('./lib/viewTemplate');
 const STATIC_FOLDER = `${__dirname}/public`;
 const SYMBOLS = require('./lib/symbols');
 
-const serveStaticFile = (req, optionalUrl) => {
+const serveStaticFile = (req, res, optionalUrl) => {
   const path = `${STATIC_FOLDER}${optionalUrl || req.url}`;
   const stat = fs.existsSync(path) && fs.statSync(path);
-  if (!stat || !stat.isFile()) return new Response();
+  if (!stat || !stat.isFile()) return serveNotFoundPage(req, res);
   const [, extension] = path.match(/.*\.(.*)$/) || [];
   const contentType = CONTENT_TYPES[extension];
   const content = fs.readFileSync(path);
-  const res = new Response();
   res.setHeader('Content-Type', contentType);
-  res.setHeader('Content-Length', content.length);
-  res.statusCode = 200;
-  res.body = content;
-  return res;
+  res.end(content);
 }
 
 const loadComments = function() {
@@ -28,12 +23,10 @@ const loadComments = function() {
   return [];
 };
 
-const redirectTo = function(url) {
-  const res = new Response();
-  res.setHeader('Location', '/guestBook.html');
-  res.setHeader('Content-Length', 0);
+const redirectTo = function(res, url) {
+  res.setHeader('Location', url);
   res.statusCode = 301;
-  return res;
+  res.end();
 }
 
 const replaceUnknownChars = function(text, character) {
@@ -41,15 +34,27 @@ const replaceUnknownChars = function(text, character) {
   return text.replace(regEx, SYMBOLS[character]);
 };
 
-const saveCommentAndRedirect = function(req) {
+const pickupParams = (query,keyValue)=>{
+  const [key,value] = keyValue.split('=');
+  query[key] = value;
+  return query;
+};
+
+const readParams = keyValueTextPairs => keyValueTextPairs.split('&').reduce(pickupParams,{});
+
+const saveCommentAndRedirect = function(req, res) {
   const comments = loadComments();
   const date = new Date();
-  const {name, comment} = req.body;
-  const keys = Object.keys(SYMBOLS);
-  const [nameText, commentText] = [name, comment].map(text => keys.reduce(replaceUnknownChars, text));
-  comments.push({date, name: nameText, comment: commentText});
-  fs.writeFileSync('./data/comments.json',JSON.stringify(comments), 'utf8');
-  return redirectTo('/guestBook.html');
+  let data = '';
+  req.on('data', text => data += text);
+  req.on('end', () => {
+    const {name, comment} = readParams(data);
+    const keys = Object.keys(SYMBOLS);
+    const [nameText, commentText] = [name, comment].map(text => keys.reduce(replaceUnknownChars, text));
+    comments.push({date, name: nameText, comment: commentText});
+    fs.writeFileSync('./data/comments.json',JSON.stringify(comments), 'utf8');
+    return redirectTo(res, '/guestBook.html');
+  });
 };
 
 const generateComment = function(commentsHtml, commentDetail) {
@@ -66,19 +71,20 @@ const generateComments = () => {
   return comments.reduce(generateComment,'');
 };
 
-const serveHomePage = function(req) {
-  return serveStaticFile(req, '/home.html');
+const serveHomePage = function(req, res) {
+  return serveStaticFile(req, res, '/home.html');
 };
 
-const serveGuestBookPage = function(req) {
+const serveGuestBookPage = function(req, res) {
   const commentsHtml = generateComments();
   const html = loadTemplate('guestBook.html', {COMMENTS: commentsHtml});
-  const res = new Response();
   res.setHeader('Content-Type', CONTENT_TYPES.html);
-  res.setHeader('Content-Length', html.length);
-  res.statusCode = 200;
-  res.body = html;
-  return res;
+  res.end(html);
+}
+
+const serveNotFoundPage = function(req, res) {
+  res.statusCode = 404;
+  res.end('Not Found');
 }
 
 const findHandler = (req) => {
@@ -86,11 +92,11 @@ const findHandler = (req) => {
   if(req.method === 'POST' && req.url === '/saveComment') return saveCommentAndRedirect;
   if(req.method === 'GET' && req.url === '/guestBook.html') return serveGuestBookPage;
   if(req.method === 'GET') return serveStaticFile;
-  return () => new Response();
+  return serveNotFoundPage;
 }
-const processRequest = (req) => {
+const processRequest = (req, res) => {
   const handler = findHandler(req);
-  return handler(req);
+  return handler(req, res);
 }
 
 module.exports = {processRequest};
