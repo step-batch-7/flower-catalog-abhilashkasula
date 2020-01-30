b589bffe1ed539e29fb7cdbe2e200f3e2d66942e
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-01-30 12:32:35 +0530

    using visibility style instead of opacity

--- public/js/hideWaterJug.js
+++ public/js/hideWaterJug.js
@@ -1,9 +1,9 @@
 const hide = function() {
   const jug = document.querySelector('#can');
   const milliSeconds = 1000;
-  jug.style['opacity'] = '0%';
+  jug.style['visibility'] = 'hidden';
   const setOpacityToHigh = () => {
-    jug.style['opacity'] = '100%';
+    jug.style['visibility'] = 'visible';
   };
   setTimeout(setOpacityToHigh, milliSeconds);
 };
--- server.js
+++ server.js
@@ -8,9 +8,12 @@ const app = new App();
 app.get('/guestBook.html', handlers.serveGuestBookPage);
 app.get('', handlers.serveStaticFile);
 app.get('', handlers.serveNotFound);
+
 app.use(handlers.readBody);
+
 app.post('/saveComment', handlers.saveCommentAndRedirect);
 app.post('', handlers.serveNotFound);
+
 app.use(handlers.serveBadRequest);
 
 const main = (port = defaultPort) => {
