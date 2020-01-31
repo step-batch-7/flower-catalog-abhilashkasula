b97d23d08f04f6c3ab665e6cca826dfa67de1ca9
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-01-31 14:46:30 +0530

    added tests for homepage

--- test/handlersTest.js
+++ test/handlersTest.js
@@ -5,16 +5,54 @@ const statusCodes = {
   'OK': 200
 };
 
-describe('GET homepage', function() {
-  it('should should get the homepage / path', function(done) {
+describe('GET homepage', () => {
+  it('should get home.html / path', (done) => {
     request(app.handleRequest.bind(app))
       .get('/')
       .set('Accept', '*/*')
       .expect(statusCodes.OK)
       .expect('Content-Type', 'text/html')
-      .expect('Content-length', '815', done)
+      .expect('Content-length', '815')
       .expect(/abeliophyllum.html/)
       .expect(/ageratum.html/)
-      .expect(/guestBook.html/);
+      .expect(/guestBook.html/, done);
+  });
+  
+  it('should get index.css for /css/index.css path', (done) => {
+    request(app.handleRequest.bind(app))
+      .get('/css/index.css')
+      .set('Accept', '*/*')
+      .expect(statusCodes.OK)
+      .expect('Content-Type', 'text/css')
+      .expect('Content-length', '787')
+      .expect(/.header {/, done);
+  });
+
+  it('should get hideWaterJug.js for /js/hideWaterJug.js path', (done) => {
+    request(app.handleRequest.bind(app))
+      .get('/js/hideWaterJug.js')
+      .set('Accept', '*/*')
+      .expect(statusCodes.OK)
+      .expect('Content-Type', 'application/javascript')
+      .expect('Content-length', '269')
+      .expect(/hide /, done);
+  });
+
+  it('should get freshorigins image for /images/freshorigins.jpg', (done) => {
+    request(app.handleRequest.bind(app))
+      .get('/images/freshorigins.jpg')
+      .set('Accept', '*/*')
+      .expect(statusCodes.OK)
+      .expect('Content-Type', 'image/jpg')
+      .expect('Content-length', '381314', done);
+  });
+
+  it('should get water-can gif for /images/watering-can.gif', (done) => {
+    request(app.handleRequest.bind(app))
+      .get('/images/watering-can.gif')
+      .set('Accept', '*/*')
+      .expect(statusCodes.OK)
+      .expect('Content-Type', 'image/gif')
+      .expect('Content-length', '65088', done);
   });
 });
